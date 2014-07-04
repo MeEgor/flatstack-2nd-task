779be6e8a9d4790021eb2f8b684e702db8e7e03c
Konnov Egor <egorknv@gmail.com>
2014-07-04 21:54:39 +1100

    action_mailer heroku fix-1

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -3,12 +3,22 @@ class UserMailer < ActionMailer::Base
 
   def welcome_email user
     @user = user
-    @url  = 'http://localhost:3000'
+    if Rails.env.development?
+      @url  = 'http://localhost:3000'
+    else
+      @url = 'http://damp-oasis-6053.herokuapp.com'
+    end
+
     mail(to: @user.email, subject: 'Добро пожаловать на "Дарим вместе"')
   end
 
   def verify_email user
-    @url = 'http://localhost:3000'
+    if Rails.env.development?
+      @url = 'http://localhost:3000'
+    else
+      @url = 'http://damp-oasis-6053.herokuapp.com'
+    end
+
     @verify_email_url = "#{@url}/#/profile?verify_email_token=#{user.email_confirmation_token}"
 
     mail(to: user.email, subject: 'Подтверждение e-mail: "Календарь"')
